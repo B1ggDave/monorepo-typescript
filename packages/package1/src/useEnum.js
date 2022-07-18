fe38fcfa229936121f3b2c3d012ad605affbb996
Anton Kropp <anton@getcopper.com>
2022-07-18 09:42:00 -0700

    init

---
+++ packages/package1/src/useEnum.js
@@ -0,0 +1,9 @@
+"use strict";
+// import { Foo } from "package2/enum";
+//
+// const x: Foo
+//
+// switch(x) {
+//
+// }
+//# sourceMappingURL=useEnum.js.map
---
+++ packages/package1/src/useEnum.ts
@@ -0,0 +1,7 @@
+// import { Foo } from "package2/enum";
+//
+// const x: Foo
+//
+// switch(x) {
+//
+// }
